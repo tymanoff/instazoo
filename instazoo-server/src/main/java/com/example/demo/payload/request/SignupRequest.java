f82bf5936fdea1dc0bedec8b2453c6cfcaaeaa11
tymanoff <djdkdancik@gmail.com>
2022-07-28 21:08:15 +0400

    add JWTAuthenticationEntryPoint, add LoginRequest, add SignupRequest, add SecurityConfig, add InvalidLoginResponse

---
+++ instazoo-server/src/main/java/com/example/demo/payload/request/SignupRequest.java
@@ -0,0 +1,24 @@
+package com.example.demo.payload.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class SignupRequest {
+
+    @Email(message = "It should have email format")
+    @NotBlank(message = "User email is required")
+    private String email;
+    @NotBlank(message = "Please enter your name")
+    private String firstname;
+    @NotBlank(message = "Please enter your lastname")
+    private String lastname;
+    @NotBlank(message = "Please enter your username")
+    private String username;
+    @NotBlank(message = "Password is required")
+    private String password;
+    private String confirmPassword;
+
+}
