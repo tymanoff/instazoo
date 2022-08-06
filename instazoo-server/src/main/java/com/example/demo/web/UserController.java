ca15fa0d4c1589e120b3ae55f81898b45043e8a6
tymanoff <djdkdancik@gmail.com>
2022-08-06 12:40:44 +0400

    add UserController

--- instazoo-server/src/main/java/com/example/demo/web/UserController.java
+++ instazoo-server/src/main/java/com/example/demo/web/UserController.java
@@ -43,6 +43,7 @@ public class UserController {
         return new ResponseEntity<>(userDTO, HttpStatus.OK);
     }
 
+    @PostMapping("/update")
     public ResponseEntity<Object> updateUser(@Valid @RequestBody UserDTO userDTO, BindingResult bindingResult, Principal principal){
         ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
         if(!ObjectUtils.isEmpty(errors)) return errors;
@@ -52,6 +53,4 @@ public class UserController {
         UserDTO userUpdate = userFacade.userToUserDTO(user);
         return new ResponseEntity<>(userUpdate, HttpStatus.OK);
     }
-
-
 }
