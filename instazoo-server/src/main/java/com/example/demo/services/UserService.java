cf91f9703b1c2ed582aec3fa141388c8880654e6
tymanoff <djdkdancik@gmail.com>
2022-08-06 12:39:30 +0400

    add UserController

--- instazoo-server/src/main/java/com/example/demo/services/UserService.java
+++ instazoo-server/src/main/java/com/example/demo/services/UserService.java
@@ -65,4 +65,7 @@ public class UserService {
                 .orElseThrow(() -> new UsernameNotFoundException("Username not found with username " + username));
     }
 
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 }
